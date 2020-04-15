b7bf3c27d712d0f3e8287a1fa76b08a4a1a920e5
Zaki <zakirjefferson@gmail.com>
2020-04-15 09:42:18 -0400

    web app

--- Image-Classification/WebApp/firearm.py
+++ Image-Classification/WebApp/firearm.py
@@ -1,34 +1,8 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-import seaborn as sns
 import numpy as np
 import pandas as pd
-import requests
-import json
 import math
-import sklearn
-from scipy import stats
-from scipy.stats import norm
-from sklearn.utils import resample
-import pickle
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
-import scipy.stats as stats
-import random
-from collections import Counter
-from statsmodels.stats.outliers_influence import variance_inflation_factor
-from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV, cross_val_score, cross_validate
-from sklearn.linear_model import LassoCV, Lasso, Ridge, LinearRegression, LogisticRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import (roc_curve, auc, confusion_matrix, roc_auc_score,
-                          precision_recall_curve, precision_recall_fscore_support)
-import scipy.stats as stats
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.ensemble import RandomForestClassifier
-from imblearn.over_sampling import SMOTE, ADASYN
-from pprint import pprint
+
 import keras
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
@@ -38,7 +12,6 @@ from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 from keras.utils import np_utils
 from keras import backend, layers, models
 from PIL import Image
-import imageio
 import os
 import keras.backend.tensorflow_backend as tb
 
