b656c70a8b492c2992fdace195dd53691120f0ba
Zaki <zakirjefferson@gmail.com>
2020-11-16 21:04:09 -0500

    Python Script

---
+++ Image-Classification/Script/script.py
@@ -0,0 +1,261 @@
+# ----------------------------Imports---------------------------
+# Neural Network
+from tensorflow.keras.applications import imagenet_utils
+from keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.models import load_model
+import keras
+import tensorflow as tf
+
+# For measuring the inference time.
+import time
+import random
+
+# Computer Vision
+import cv2
+
+# Graphing
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Math
+import numpy as np
+
+# File handling
+import pickle
+from os import listdir
+from os.path import isfile, join
+import os
+# -------------------------------------------------------------
+
+# ------------------------Loading Images-----------------------
+# Alters pictures
+image_gen = keras.preprocessing.image.ImageDataGenerator(
+    featurewise_center=False,
+    samplewise_center=False,
+    featurewise_std_normalization=False,
+    samplewise_std_normalization=False,
+    zca_whitening=False,
+    zca_epsilon=1e-06,
+    rotation_range=0.0,
+    width_shift_range=0.0,
+    height_shift_range=0.0,
+    shear_range=0.0,
+    zoom_range=0.0,
+    channel_shift_range=0.0,
+    fill_mode='nearest',
+    cval=0.0,
+    horizontal_flip=False,
+    vertical_flip=False,
+    rescale=1./255,
+    preprocessing_function=None,
+    data_format=None)
+
+# Loading Images
+image_gen = image_gen.flow_from_directory(
+    "Data-Images",
+    target_size=(300, 300),
+    color_mode='rgb',
+    classes=None,
+    class_mode='categorical',
+    batch_size=2591,
+    shuffle=True,
+    seed=123,
+    save_to_dir=None,
+    save_prefix='',
+    save_format='png',
+    follow_links=False,
+    interpolation='nearest')
+
+# getting images and labels
+images, labels = next(image_gen)
+
+# -------------------------Functions------------------------
+def display_image(image_num):
+    """
+    Prints out picture of
+    the image that is selected
+    
+    After the else statement,
+    we try to use the file name
+    in oder to print out the image,
+    
+    while the first image is used to 
+    print the image directly without a 
+    filename.
+    """
+    try:
+        fig = plt.figure(figsize=(20, 15))
+        plt.grid(False)
+        plt.imshow(images[image_num])
+    except (RuntimeError, TypeError, NameError):
+        print("[INFO] Could not print image")
+        print("[INFO] trying something else...")
+    else:
+        print("[INFO] returning image...")
+        # Image path - getting images on file
+        image_paths = "Demo-Images/"
+        image_select = image_paths + ([f for f in listdir(image_paths) if isfile(join(image_paths, f))][image_num])  # Other way instead of the listdir function
+        img = plt.imread(fname=image_select)
+        plt.imshow(img)
+
+		
+def listdirimage(path):
+    """
+    Checking for file path
+    of images
+    """
+    lst = []
+    if os.path.isdir(path): # Check this path = path to folder
+        for file in os.listdir(path):  
+            file_path = os.path.join(path, file)  
+            lst.append(file_path)
+    return lst
+
+
+model_path = "model_3.hdf5"
+cnn_model = keras.models.load_model(model_path)
+
+# Image Classes
+classes = ['Assault Rifle', 'Handgun', 'No Firearm']
+
+# Prediction Function
+def predict(model, image_num):
+    # Image path - getting images on file
+    image_paths = "Demo-Images/"
+    image_select = image_paths + ([f for f in listdir(image_paths) if isfile(join(image_paths, f))])[image_num] # Other way instead of the listdir function
+    
+    img = load_img(image_select, target_size=(300, 300))  # Loading image
+    img = img_to_array(img)  # Transforming image to array
+    img = img / 255  # Normalizing Image
+    img = np.expand_dims(img, axis=0)  # Expanding dimensions
+    predict = cnn_model.predict(img)  # Predicting the image
+    pred_name = classes[np.argmax(predict)]  # Predicting the name
+    prediction = str(round(predict.max() * 100, 3))
+    print(display_image(image_num=image_num))
+    return prediction + '%', pred_name
+
+
+# Prediction Function
+def predict_region_of_proposals(model, proposals_used):
+    """
+    predicts regions of proposals
+    """
+    predict = model.predict(proposals_used)  # Predicting the image
+    for proposals in predict:
+        pred_name = classes[np.argmax(proposals)]  # Predicting the name
+    prediction = str(round(predict.max() * 100, 3))
+    print(pred_name)
+    return predict
+
+
+def compute_iou(boxA, boxB):
+    """
+    IOU is a form of 
+    performance measurement
+    for our object detector.
+    """
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the intersection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    # return the intersection over union value
+    return iou
+
+
+#  Felzenszwalb et al.
+def example_non_max_suppression(boxes, overlapThresh):
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
+    # initialize the list of picked indexes
+    pick = []
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:,0]
+    y1 = boxes[:,1]
+    x2 = boxes[:,2]
+    y2 = boxes[:,3]
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(y2)
+	
+
+def non_max_suppression(boxes, probs=None, overlapThresh=0.3):
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
+
+    # if the bounding boxes are integers, convert them to floats -- this
+    # is important since we'll be doing a bunch of divisions
+    if boxes.dtype.kind == "i":
+        boxes = boxes.astype("float")
+
+    # initialize the list of picked indexes
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    # compute the area of the bounding boxes and grab the indexes to sort
+    # (in the case that no probabilities are provided, simply sort on the
+    # bottom-left y-coordinate)
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = y2
+
+    # if probabilities are provided, sort on them instead
+    if probs is not None:
+        idxs = probs
+
+    # sort the indexes
+    idxs = np.argsort(idxs)
+
+    # keep looping while some indexes still remain in the indexes list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the index value
+        # to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of the bounding
+        # box and the smallest (x, y) coordinates for the end of the bounding
+        # box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # delete all indexes from the index list that have overlap greater
+        # than the provided overlap threshold
+        idxs = np.delete(idxs, np.concatenate(([last],
+            np.where(overlap > overlapThresh)[0])))
+
+    # return only the bounding boxes that were picked
+    return boxes[pick].astype("int")
+# ----------------------------------------------------------------------
+
+# -------------------------OpenCV Work-----------------------------------
