223157a2fe258608439d4554e512d4a00ede32fa
Zaki <zakirjefferson@gmail.com>
2020-11-22 04:47:06 -0500

    NMS

--- Image-Classification/Script/script.py
+++ Image-Classification/Script/script.py
@@ -139,9 +139,9 @@ def predict(model, image_num):
 
 
 # Prediction Function
-def predict_region_of_proposals(model, proposals_used):
+def predict_region_of_interest(model, proposals_used):
     """
-    predicts regions of proposals
+    predicts region proposals
     """
     predict = model.predict(proposals_used)  # Predicting the image
     for proposals in predict:
@@ -192,7 +192,46 @@ def example_non_max_suppression(boxes, overlapThresh):
     # boxes by the bottom-right y-coordinate of the bounding box
     area = (x2 - x1 + 1) * (y2 - y1 + 1)
     idxs = np.argsort(y2)
-	
+
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list, add the index
+        # value to the list of picked indexes, then initialize
+        # the suppression list (i.e. indexes that will be deleted)
+        # using the last index
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+        suppress = [last]
+
+        # loop over all indexes in the indexes list
+        for pos in range(0, last):
+            # grab the current index
+            j = idxs[pos]
+            # find the largest (x, y) coordinates for the start of
+            # the bounding box and the smallest (x, y) coordinates
+            # for the end of the bounding box
+            xx1 = max(x1[i], x1[j])
+            yy1 = max(y1[i], y1[j])
+            xx2 = min(x2[i], x2[j])
+            yy2 = min(y2[i], y2[j])
+            # compute the width and height of the bounding box
+            w = max(0, xx2 - xx1 + 1)
+            h = max(0, yy2 - yy1 + 1)
+            # compute the ratio of overlap between the computed
+            # bounding box and the bounding box in the area list
+            overlap = float(w * h) / area[j]
+            # if there is sufficient overlap, suppress the
+            # current bounding box
+            if overlap > overlapThresh:
+                suppress.append(pos)
+        # delete all indexes from the index list that are in the
+        # suppression list
+        idxs = np.delete(idxs, suppress)
+    # return only the bounding boxes that were picked
+    return boxes[pick]
+
 
 def non_max_suppression(boxes, probs=None, overlapThresh=0.3):
     # if there are no boxes, return an empty list
